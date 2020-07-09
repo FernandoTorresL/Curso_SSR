bd9f6b45ffa43c8f0558f4b9807fdb033fbd89f3
Fernando Torres <contacto@fertorresmx.dev>
2020-07-09 01:17:37 -0500

    Changes to principal functioin to render from server

--- src/server/server.js
+++ src/server/server.js
@@ -1,41 +1,64 @@
 /* eslint-disable global-require */
-import express from 'express';
-import dotenv from 'dotenv';
-import webpack from 'webpack';
+import express from "express";
+import dotenv from "dotenv";
+import webpack from "webpack";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { renderRoutes } from "react-router-config";
+import { StaticRouter } from "react-router-dom";
+import serverRoutes from "../frontend/routes/serverRoutes";
+import reducer from "../frontend/reducers/index";
+import initialState from "../frontend/initialState";
 
 dotenv.config();
 
 const { ENV, PORT } = process.env;
 const app = express();
 
-if (ENV === 'development') {
-  console.log('Development config');
-  const webpackConfig = require('../../webpack.config');
-  const webpackDevMiddleware = require('webpack-dev-middleware');
-  const webpackHotMiddleware = require('webpack-hot-middleware');
+if (ENV === "development") {
+  console.log("Development config");
+  const webpackConfig = require("../../webpack.config");
+  const webpackDevMiddleware = require("webpack-dev-middleware");
+  const webpackHotMiddleware = require("webpack-hot-middleware");
   const compiler = webpack(webpackConfig);
   const serverConfig = { port: PORT, hot: true };
 
   app.use(webpackDevMiddleware(compiler, serverConfig));
   app.use(webpackHotMiddleware(compiler));
-
 }
 
-app.get('*', (req, res) => {
-  res.send(`
-  <!DOCTYPE html>
+const setResponse = (html) => {
+  return `
+    <!DOCTYPE html>
     <html>
       <head>
         <link rel="stylesheet" href="assets/app.css" type="text/css">
         <title>Platzi Video</title>
       </head>
       <body>
-        <div id="app"></div>
+        <div id="app">${html}</div>
         <script src="assets/app.js" type="text/javascript"></script>
       </body>
     </html>
-  `);
-});
+  `;
+};
+
+const renderApp = (req, res) => {
+  const store = createStore(reducer, initialState);
+  const html = renderToString(
+    <Provider store={store}>
+      <StaticRouter location={req.url} context={{}}>
+        {renderRoutes(serverRoutes)}
+      </StaticRouter>
+    </Provider>
+  );
+
+  res.send(setResponse(html));
+};
+
+app.get("*", renderApp);
 
 app.listen(PORT, (err) => {
   if (err) console.log(err);
